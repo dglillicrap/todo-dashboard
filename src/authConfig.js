64fc2c8b4b0442b350f38557454621698dd55c6a
dglillicrap <109191027+dglillicrap@users.noreply.github.com>
2025-10-08 05:42:19 +1000

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,18 +1,6 @@
 const msalConfig = {
   auth: {
-    clientId: '19f19e6c-8ddc-44c0-b1fd-7b0e8a549d7b',
-    authority: 'https://login.microsoftonline.com/common',
-    redirectUri: 'http://localhost:3000',
-  },
-  cache: {
-    cacheLocation: 'localStorage',
-    storeAuthStateInCookie: false,
-  }
-};
-
-export default {
-  auth: {
-    clientId: "<same Azure client ID>",
+    clientId: "19f19e6c-8ddc-44c0-b1fd-7b0e8a549d7b",
     authority: "https://login.microsoftonline.com/common",
     redirectUri: "https://todo-dgl.onrender.com"
   },
@@ -21,3 +9,5 @@ export default {
     storeAuthStateInCookie: false
   }
 };
+
+export default msalConfig;
